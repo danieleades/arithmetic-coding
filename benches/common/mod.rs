f05765f2c2a8ad008f9515c0090a895bac33379f
Daniel Eades <danieleades@hotmail.com>
2022-03-23 07:10:03 +0000

    everything is working

---
+++ benches/common/mod.rs
@@ -0,0 +1,70 @@
+use arithmetic_coding::{Decoder, Encoder, Model};
+use bitstream_io::{BigEndian, BitReader, BitWrite, BitWriter};
+
+#[allow(unused)]
+pub fn round_trip<M>(model: M, input: &[M::Symbol])
+where
+    M: Model + Clone,
+    M::Symbol: Copy,
+{
+    let buffer = encode(model.clone(), input.iter().copied());
+
+    for symbol in decode(model, &buffer) {
+        continue;
+    }
+}
+
+pub fn encode<M, I>(model: M, input: I) -> Vec<u8>
+where
+    M: Model,
+    I: IntoIterator<Item = M::Symbol>,
+{
+    let mut bitwriter = BitWriter::endian(Vec::new(), BigEndian);
+    let mut encoder = Encoder::<M>::new(model);
+
+    encoder.encode_all(input, &mut bitwriter).unwrap();
+    bitwriter.byte_align().unwrap();
+
+    bitwriter.into_writer()
+}
+
+pub fn decode<M>(model: M, buffer: &[u8]) -> Vec<M::Symbol>
+where
+    M: Model,
+{
+    let bitreader = BitReader::endian(buffer, BigEndian);
+    let mut decoder = Decoder::new(model, bitreader).unwrap();
+    let mut output = Vec::new();
+
+    while let Some(symbol) = decoder.decode_symbol().unwrap() {
+        output.push(symbol);
+    }
+    output
+}
+
+#[allow(unused)]
+pub fn round_trip_string<M>(model: M, input: String)
+where
+    M: Model<Symbol = char> + Clone,
+{
+    let input_bytes = input.bytes().len();
+
+    let buffer = encode(model.clone(), input.chars());
+
+    let output_bytes = buffer.len();
+
+    println!("input bytes: {}", input_bytes);
+    println!("output bytes: {}", output_bytes);
+
+    println!(
+        "compression ratio: {}",
+        input_bytes as f32 / output_bytes as f32
+    );
+
+    let output = decode(model, &buffer);
+
+    let mut prefix: String = output.into_iter().take(299).collect();
+    prefix.push_str("...");
+
+    println!("{}", prefix);
+}
---
+++ benches/sherlock.rs
@@ -0,0 +1,75 @@
+use std::{fs::File, io::Read, ops::Range};
+
+use arithmetic_coding::{
+    Model,
+};
+
+mod common;
+
+use fenwick_model::simple::{FenwickModel, ValueError};
+
+#[derive(Debug, Clone)]
+pub struct StringModel {
+    fenwick_model: FenwickModel,
+}
+
+impl StringModel {
+    #[must_use]
+    pub fn new(symbols: usize) -> Self {
+        let fenwick_model = FenwickModel::with_symbols(symbols);
+        Self { fenwick_model }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("invalid character: {0}")]
+pub struct Error(char);
+
+impl Model for StringModel {
+    type B = u64;
+    type Symbol = u8;
+    type ValueError = ValueError;
+
+    fn probability(
+        &self,
+        symbol: Option<&Self::Symbol>,
+    ) -> Result<Range<Self::B>, Self::ValueError> {
+        let fenwick_symbol = symbol.map(|c| *c as usize);
+        self.fenwick_model.probability(fenwick_symbol.as_ref())
+    }
+
+    fn symbol(&self, value: Self::B) -> Option<Self::Symbol> {
+        self.fenwick_model.symbol(value).map(|x| x as u8)
+    }
+
+    fn max_denominator(&self) -> Self::B {
+        self.fenwick_model.max_denominator()
+    }
+
+    fn denominator(&self) -> Self::B {
+        self.fenwick_model.denominator()
+    }
+}
+
+fn round_trip(input: &[u8]) {
+    let model = StringModel::new(256);
+
+    common::round_trip(model, input);
+}
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut input_string = String::new();
+    File::open("./benches/sherlock.txt")
+        .unwrap()
+        .read_to_string(&mut input_string)
+        .unwrap();
+
+    let input = input_string.into_bytes();
+
+    c.bench_function("round trip", |b| b.iter(|| round_trip(black_box(&input))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
