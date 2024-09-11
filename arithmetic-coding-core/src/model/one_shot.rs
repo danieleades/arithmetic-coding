30fc8e20e771be560454ef90ffdee067a23a4e18
danieleades <33452915+danieleades@users.noreply.github.com>
2024-09-11 07:24:04 +0100

    docs(lints): address clippy lint errors related to doc comments (#65)

--- arithmetic-coding-core/src/model/one_shot.rs
+++ arithmetic-coding-core/src/model/one_shot.rs
@@ -5,8 +5,10 @@ use std::ops::Range;
 pub use crate::fixed_length::Wrapper;
 use crate::{fixed_length, BitStore};
 
-/// A [`Model`] is used to calculate the probability of a given symbol occuring
-/// in a sequence. The [`Model`] is used both for encoding and decoding. A
+/// A [`Model`] is used to calculate the probability of a given symbol occurring
+/// in a sequence.
+///
+/// The [`Model`] is used both for encoding and decoding. A
 /// 'one-shot' only ever encodes a single symbol, and so does
 /// not need to encode an EOF symbol.
 ///
